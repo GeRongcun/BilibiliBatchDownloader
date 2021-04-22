608fe39818209f67e170485956d73ed182b9b16d
gerongcun <gerongcun@gmail.com>
2021-04-22 21:30:42 +0800

    修改renameFiles.py

--- core/downloadVideos.py
+++ core/downloadVideos.py
@@ -17,22 +17,20 @@ def downloadVideo(url,downloadFolder):
     return os.system(command)
     # 返回0，下载成功；返回1，下载失败
 
-
 # 批量下载视频
 # excelFile：存放视频url的excel表格文件
-# downloadFolder：视频下载到哪个文件夹
+# resultFolder：存放所有收藏夹或投稿空间的视频
 # videoType：视频类型，0：收藏夹视频，1：投稿视频
-# beginTime：视频最早发布时间，默认值是'19700101'
-def BatchDownloadVideos(excelFile,downloadFolder,videoType,beginTime='19700101'):
+def BatchDownloadVideos(excelFile,resultFolder,videoType):
     # os.path.split(excelFile)
     # ('D:\\00Gerc\\XueXi20200104\\00Desktop\\我的笔记20200311\\00葛荣存\\个人项目笔记\\20210418批量下载B站视频
     # \\Github项目\\result\\00视频信息Excel文件', '投稿_回形针PaperClip_20210420.xlsx')
     
-    # 在downloadFolder下创建新文件夹，用于存放本次下载的所有视频
-    downloadFolder=os.path.join(downloadFolder,os.path.split(excelFile)[1][:-5])
+    # 在resultFolder下创建新文件夹，用于存放单个收藏夹或投稿空间的视频
+    downloadFolder_videoList=os.path.join(resultFolder,os.path.split(excelFile)[1][:-5])
     # 投稿_回形针PaperClip_20210420
-    if not os.path.exists(downloadFolder):
-        os.mkdir(downloadFolder)
+    if not os.path.exists(downloadFolder_videoList):
+        os.mkdir(downloadFolder_videoList)
 
     # 读取excel表格
     df=pd.read_excel(excelFile, sheet_name=0, header=None, index_col=None,na_values=['NA'],dtype='str')
@@ -61,33 +59,37 @@ def BatchDownloadVideos(excelFile,downloadFolder,videoType,beginTime='19700101')
         bvid=record[5]
         # UP主姓名
         upper=record[6]
+        # 删除UP主姓名中的下划线，因为下划线是本程序的分隔符
+        upper=upper.replace("_","")
         
         # 确定视频时间，跟beginTime比较
         # videoType，0：收藏夹视频，1：投稿视频
+        # videoType=1
         if videoType==0:
             timeVideo=mtime
         else:
             timeVideo=pubtime
-        if timeVideo<beginTime:
-            break
         
         # 输出信息，方便了解视频信息
         # len(records)-1，第一行是列名
-        print("序号：%d//%d，名称：%s，时长：%s" % (orderVideo,len(records)-1,title,duration))
+        print("序号：%d/%d，名称：%s，时长：%s" % (orderVideo,len(records)-1,title,duration))
         # 拼合出视频url
         video_url='https://www.bilibili.com/video/'+bvid
         # 文件夹位置，为视频创建一个文件夹
-        folderName=str(pubtime)+'_'+upper+'_'+title+'_'+'B站'+'_'+bvid
-        folder=os.path.join(downloadFolder,folderName)
+        folderName=str(timeVideo)+'_'+upper+'_'+title+'_'+'B站'+'_'+bvid
+        folder=os.path.join(downloadFolder_videoList,folderName)
         # 下载视频，并判断是否下载成功
         isSuccess=downloadVideo(video_url,folder)
         if isSuccess==0:
-            record[6]='0'
+            record[7]='0'
         else:
-            record[6]=str(isSuccess)
+            record[7]=str(isSuccess)
+        record
     # 写入Excel文件
     data_excel = pd.DataFrame(records)
     writer = pd.ExcelWriter(excelFile)
     data_excel.to_excel(writer, index=None, header=None)
     writer.save()
     writer.close()
+    # 存放单个收藏夹或投稿空间的视频
+    return downloadFolder_videoList
--- core/main.py
+++ core/main.py
@@ -2,7 +2,7 @@
 import os
 
 # 项目文件夹位置
-projectFolder=r'D:/00Gerc/XueXi20200104/00Desktop/我的笔记20200311/00葛荣存/个人项目笔记/20210418批量下载B站视频/Github项目'
+projectFolder=r'D:\00Gerc\XueXi20200104\00Desktop\我的笔记20200311\00葛荣存\个人项目笔记\20210418批量下载B站视频\批量下载B站视频'
 # 代码文件夹位置
 coreFolder=os.path.join(projectFolder,'core')
 os.chdir(coreFolder) # 打开文件目录，相当于cd
@@ -10,40 +10,53 @@ os.chdir(coreFolder) # 打开文件目录，相当于cd
 # 导入自定义模块
 import getVideoUrl
 import downloadVideos
+import renameFiles
 
-# 视频下载到哪个文件夹
-downloadFolder=os.path.join(projectFolder,'result')
+# 存放所有收藏夹或投稿空间的视频
+resultFolder=os.path.join(projectFolder,'result')
 # excelFolder：文件夹，用于存放包含视频url的excel表格文件
-excelFolder=os.path.join(downloadFolder,'00视频信息Excel文件')
+excelFolder=os.path.join(resultFolder,'00视频信息Excel文件')
 
 # 根据收藏夹url，批量下载视频
-def downloadFavlistVideos(fav_url,beginTime='19700101'):
+def downloadFavlistVideos(fav_url,beginTime='19700101',endTime=getVideoUrl.getDateStr()):
+    # beginTime='19700101'
+    # endTime='20210421'
     # 根据收藏夹url，获得API_url
     fav_API_url=getVideoUrl.getFavlistAPI_url(fav_url)
     # 收藏夹视频API，批量获得收藏夹视频的url
-    excelFile=getVideoUrl.getFavlistVideoUrl(fav_API_url,excelFolder)
+    excelFile=getVideoUrl.getFavlistVideoUrl(fav_API_url,excelFolder,beginTime,endTime)
     # 批量下载视频
-    downloadVideos.BatchDownloadVideos(excelFile,downloadFolder,0,beginTime)
+    # 存放单个收藏夹或投稿空间的视频
+    downloadFolder_videoList=downloadVideos.BatchDownloadVideos(excelFile,resultFolder,0)
+    return downloadFolder_videoList
 
 # 根据投稿空间url，批量下载视频
-def downloadContributionVideos(contri_url,beginTime='19700101'):
+def downloadContributionVideos(contri_url,beginTime='19700101',endTime=getVideoUrl.getDateStr()):
+    # beginTime='19700101'
+    # endTime='20210421'
     # UP主投稿空间url，获得API_url
     contri_API_url=getVideoUrl.getContriAPI_url(contri_url)
     # 投稿空间API，批量获得投稿视频的url
-    excelFile=getVideoUrl.getContriVideoUrl(contri_API_url,excelFolder)
-    # 批量下载视频
-    downloadVideos.BatchDownloadVideos(excelFile,downloadFolder,1,beginTime)
+    excelFile=getVideoUrl.getContriVideoUrl(contri_API_url,excelFolder,beginTime,endTime)
+    # 存放单个收藏夹或投稿空间的视频
+    downloadFolder_videoList=downloadVideos.BatchDownloadVideos(excelFile,resultFolder,1)
+    return downloadFolder_videoList
 
 # main函数
 def main():
-    # 收藏夹url，favlist
-    # fav_url='https://space.bilibili.com/927587/favlist?fid=1055169787&ftype=create'
-    fav_url='https://space.bilibili.com/91724487/favlist?fid=204884287&ftype=create'
-    # UP主投稿空间url，contribution
-    contri_url='https://space.bilibili.com/258150656/video?tid=0&page=2&keyword=&order=pubdate'
+    # 回形针PaperClip投稿
+    # contri_url='https://space.bilibili.com/258150656/video?tid=0&page=2&keyword=&order=pubdate'
+    # downloadFolder_videoList=downloadContributionVideos(contri_url,'20201224','20210422')
+    # renameFiles.RenameFiles(downloadFolder_videoList)
+    
+    # 划水小能手的收藏夹-学习
+    # fav_url='https://space.bilibili.com/91724487/favlist?fid=204884287&ftype=create'
+    # downloadFolder_videoList=downloadFavlistVideos(fav_url,'19700101','20210421')
+    # renameFiles.RenameFiles(downloadFolder_videoList)
 
-    downloadFavlistVideos(fav_url,'20210329')
-    downloadContributionVideos(contri_url,'20200929')
-    downloadContributionVideos(contri_url,'20210415')
+    # 划水小能手的收藏夹-驾驶
+    # fav_url='https://space.bilibili.com/91724487/favlist?fid=1204908287&ftype=create'
+    # downloadFolder_videoList=downloadFavlistVideos(fav_url,'19700101','20210422')
+    # renameFiles.RenameFiles(downloadFolder_videoList)
 if __name__ == '__main__':
     main()
